332cba55fbe43834430448cb3e51580bb276e36c
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-04-23 11:05:36 +0200

    Fix alignment

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -3,8 +3,8 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className='bg-red'>
-      <Card className='w-80 min-w-[320px] bg-red'>
+    <div className='flex h-screen items-center justify-center bg-emerald-200'>
+      <Card className='w-80 min-w-[320px]'>
         <CardHeader>
           <CardTitle>Create project</CardTitle>
           <CardDescription>Deploy your new project in one-click.</CardDescription>
