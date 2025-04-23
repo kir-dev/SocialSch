c7dc68115260c9b1fa3ae0bb5c2dae8583221056
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-04-23 10:15:17 +0200

    MainPage kezdeti próbálkozás

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -1,9 +1,22 @@
-import { HelloWorld } from '@/components/hello-world';
+import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className='flex items-center justify-center'>
-      <HelloWorld className='mt-10' />
-    </main>
+    <div className='bg-red'>
+      <Card className='w-80 min-w-[320px] bg-red'>
+        <CardHeader>
+          <CardTitle>Create project</CardTitle>
+          <CardDescription>Deploy your new project in one-click.</CardDescription>
+        </CardHeader>
+        <CardContent>
+          <h1>nagyobb</h1>
+        </CardContent>
+        <CardFooter className='flex justify-between'>
+          <Button variant='outline'>Cancel</Button>
+          <Button>Deploy</Button>
+        </CardFooter>
+      </Card>
+    </div>
   );
 }
