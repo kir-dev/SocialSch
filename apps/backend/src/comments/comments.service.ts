b96abf1f36b93cb03630f5408ca02508a83a7b20
Sáfár Erik <safar.erik13@gmail.com>
2025-04-29 19:26:15 +0200

    Define CommentEntity with validation and update CommentsService methods to return CommentEntity

--- apps/backend/src/comments/comments.service.ts
+++ apps/backend/src/comments/comments.service.ts
@@ -1,26 +1,27 @@
 import { Injectable } from '@nestjs/common';
 import { CreateCommentDto } from './dto/create-comment.dto';
 import { UpdateCommentDto } from './dto/update-comment.dto';
+import { CommentEntity } from './entities/comment.entity';
 
 @Injectable()
 export class CommentsService {
-  create(createCommentDto: CreateCommentDto) {
+  create(createCommentDto: CreateCommentDto): Promise<CommentEntity> {
     return 'This action adds a new comment';
   }
 
-  findAll() {
+  findAll(): Promise<CommentEntity> {
     return `This action returns all comments`;
   }
 
-  findOne(id: number) {
+  findOne(id: number): Promise<CommentEntity> {
     return `This action returns a #${id} comment`;
   }
 
-  update(id: number, updateCommentDto: UpdateCommentDto) {
+  update(id: number, updateCommentDto: UpdateCommentDto): Promise<CommentEntity> {
     return `This action updates a #${id} comment`;
   }
 
-  remove(id: number) {
+  remove(id: number): Promise<CommentEntity> {
     return `This action removes a #${id} comment`;
   }
 }
--- apps/backend/src/comments/entities/comment.entity.ts
+++ apps/backend/src/comments/entities/comment.entity.ts
@@ -1,1 +1,23 @@
-export class Comment {}
+import { IsBoolean, IsDate, IsInt, IsString } from 'class-validator';
+
+export class CommentEntity {
+  @IsInt()
+  commentId: number;
+  @IsInt()
+  postId: number;
+
+  @IsString()
+  authorId: string;
+
+  @IsString()
+  content: string;
+
+  @IsDate()
+  createdAt: Date;
+
+  @IsDate()
+  updatedAt: Date;
+
+  @IsBoolean()
+  visible: boolean;
+}
