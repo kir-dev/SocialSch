f11488439fce29c6cc751f737029b0c8054f6602
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-05-10 18:28:01 +0200

    Fix eslint and prettier

--- apps/frontend/src/app/(authed)/posts/page.tsx
+++ apps/frontend/src/app/(authed)/posts/page.tsx
@@ -1,6 +1,5 @@
-import { ThemeToggle } from '@/components/theme-toggle';
 import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+// import Link from 'next/link';
 import { Post } from '@/types';
 
 const posts: Post[] = [
@@ -17,9 +16,9 @@ export default async function HomePage() {
   return (
     <div className='flex items-center justify-center min-h-screen bg-secondary'>
       <Button>Click me</Button>
-      <ThemeToggle />
+      {/*<ThemeToggle />*/}
       {posts.map((post) => (
-        <PostComponent post={post} author={} title={} />
+        <PostComponent key={post.id} post={post} author='valaki' title='valami' />
       ))}
     </div>
   );
@@ -31,12 +30,12 @@ interface PostComponentProps {
   title: string;
 }
 
-function PostComponent({ post, author }: PostComponentProps) {
+function PostComponent({ post /*, author*/ }: PostComponentProps) {
   return (
     <div className='p-4 border rounded'>
       <h2 className='text-lg font-bold'>{post.title}</h2>
       <p>{post.content}</p>
-      <Link href={`/users/${post.author.id}`}>{post.author.name}</Link>
+      {/*<Link href={`/users/${post.author.id}`}>{post.author.name}</Link>*/}
     </div>
   );
 }
