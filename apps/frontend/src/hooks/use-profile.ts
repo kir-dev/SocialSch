09022282ca74545c4490cced145fac2c9b74c995
Tarjányi Bence Csanád <tarjanyicsanad@gmail.com>
2025-07-04 21:19:00 +0200

    Fix eslint and prettier

--- apps/frontend/src/hooks/use-profile.ts
+++ apps/frontend/src/hooks/use-profile.ts
@@ -2,7 +2,12 @@ import useSWR from 'swr';
 import { User } from '@/types';
 import { axiosGetFetcher } from '@/lib/fetchers';
 
-export default function useProfile() {
+export default function useProfile(): {
+  data: User | undefined;
+  error: any;
+  isLoading: boolean;
+  mutate: () => Promise<User | undefined>;
+} {
   return useSWR<User>('/auth/me', axiosGetFetcher, {
     shouldRetryOnError: false,
   });
--- apps/frontend/src/hooks/use-user.ts
+++ apps/frontend/src/hooks/use-user.ts
@@ -2,6 +2,11 @@ import useSWR from 'swr';
 import { User } from '@/types';
 import { axiosGetFetcher } from '@/lib/fetchers';
 
-export default function useProfile(id: string) {
+export default function useProfile(id: string): {
+  data: User | undefined;
+  error: any;
+  isLoading: boolean;
+  mutate: () => Promise<User | undefined>;
+} {
   return useSWR<User>(`users/${id}`, axiosGetFetcher);
 }
