f7911dc1081f261705c73dd09830783076d3b6dc
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-08 15:22:12 +0200

    Changed the layout of the post

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -8,12 +8,12 @@ export default function Home() {
   let { data: posts, isLoading } = usePosts();
 
   return (
-    <main className='flex-1 px-4 md:px-8 py-10 flex flex-col items-center space-y-6 pt-10'>
+    <>
       {isLoading && <SkeletonCard />}
       {posts &&
         posts.map((post: Post) => {
           return <PostCard key={post.postId} post={post} user={post.author} />;
         })}
-    </main>
+    </>
   );
 }
--- apps/frontend/src/components/PostCard.tsx
+++ apps/frontend/src/components/PostCard.tsx
@@ -19,7 +19,7 @@ export default function PostCard({ user, post }: PostProps) {
   }
 
   return (
-    <Card className='w-1/2'>
+    <Card>
       <PostHeader user={user} />
       <CardContent>
         <CardTitle className='pb-2 text-xl'>{post.title}</CardTitle>
