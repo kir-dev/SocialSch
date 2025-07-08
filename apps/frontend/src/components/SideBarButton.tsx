cf1526d7ad6c8753f1e0a527ff3a8b006004e340
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-08 22:39:54 +0200

    Removed the video background because it used too much memory

---
+++ apps/frontend/src/components/SideBarButton.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+export default function SideBarButton() {
+  return (
+    <>
+      <button className='bg-foreground font-bold text-xl text-background cursor-pointer rounded-2xl px-4 py-2 m-2'>
+        <span>Create Post</span>
+      </button>
+    </>
+  );
+}
