bf95bcf2a023b01899603234eec7a4c4e56d4612
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-09 11:48:15 +0200

    Átírt dolgok

--- apps/frontend/src/components/SideBarButton.tsx
+++ apps/frontend/src/components/SideBarButton.tsx
@@ -17,7 +17,13 @@ export default function SideBarButton() {
 
   return (
     <>
-      <CreatePostDialog />
+      {/*<CreatePostDialog />*/}
+      <button
+        className='bg-[url(/buttonBgImage2.jpg)] bg-cover font-bold text-xl text-white cursor-pointer rounded-2xl px-4 py-2 m-2'
+        onClick={() => handleLogin()}
+      >
+        <span>Login</span>
+      </button>
     </>
   );
 }
