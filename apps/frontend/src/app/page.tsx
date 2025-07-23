2b90141793bd1e4bad2900d036b2a0612a3c36bc
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-23 09:54:26 +0200

    Created a search bar, but it only works for the posts which are fetched from db and not created with the create function, if it was created from the create function the layout bugs

--- apps/frontend/src/app/page.tsx
+++ apps/frontend/src/app/page.tsx
@@ -3,9 +3,15 @@ import PostCard from '@/components/PostCard';
 import type { Post } from '@/types';
 import usePosts from '@/hooks/use-posts';
 import { SkeletonCard } from '@/components/SkeletonCard';
+import { Input } from '@/components/ui/input';
+import { useState } from 'react';
+import { Button } from '@/components/ui/button';
 
 export default function Home() {
   let { data: posts, isLoading } = usePosts();
+  const [filter, setFilter] = useState('');
+  const [filteredData, setFilteredData] = useState<Post[]>([]);
+  const [clicked, setClicked] = useState(false);
 
   if (!posts && !isLoading) return <h1>There are no visible posts. Come back later.</h1>;
 
@@ -13,14 +19,55 @@ export default function Home() {
     posts.sort((a, b) => new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime());
   }
 
+  function handleSubmit() {
+    setClicked((prevState) => !prevState);
+    if (clicked) {
+      setFilter('');
+      setFilteredData([]);
+      return;
+    }
+
+    if (filter === '') {
+      setFilteredData([]);
+      return;
+    }
+    let searchedData = posts?.filter((post) => post.title.toLowerCase().includes(filter.toLowerCase()));
+
+    if (searchedData) {
+      setFilteredData(searchedData);
+    }
+  }
+
   return (
-    <div className='flex flex-col items-center space-y-6 pt-10 pb-10'>
-      {/*<CreatePostSheet />*/}
+    <div className='w-full min-w-full flex flex-col items-center space-y-6 pt-10 pb-10'>
+      <div className='flex w-3/5 min-w-3/5 items-center gap-2'>
+        <Input
+          type='text'
+          placeholder='Search SocialSch'
+          className='w-4/5'
+          value={filter}
+          onChange={(e) => setFilter(e.target.value)}
+        ></Input>
+        <Button variant='outline' className='cursor-pointer' onClick={() => handleSubmit()}>
+          {clicked ? 'Cancel' : 'Search'}
+        </Button>
+      </div>
+
       {isLoading && <SkeletonCard />}
-      {posts &&
-        posts.map((post: Post) => {
-          return <PostCard key={post.postId} post={post} user={post.author} />;
-        })}
+      {filteredData.length === 0 && posts && (
+        <div className='flex flex-col w-full items-center gap-2'>
+          {posts.map((post: Post) => {
+            return <PostCard key={post.postId} post={post} user={post.author} />;
+          })}
+        </div>
+      )}
+      {filteredData.length !== 0 && (
+        <div className='flex flex-col w-full items-center gap-2'>
+          {filteredData.map((post: Post) => {
+            return <PostCard key={post.postId} post={post} user={post.author} />;
+          })}
+        </div>
+      )}
     </div>
   );
 }
