f18480fe43af9961971374ad9db79c2ab5f83a07
Vaska Barnabás <vaska.barnabas@gmail.com>
2025-07-26 15:48:22 +0200

    Created account comment section

--- apps/frontend/src/components/accountComments.tsx
+++ apps/frontend/src/components/accountComments.tsx
@@ -1,4 +1,5 @@
 import { Comment } from '@/types';
+import { format } from 'date-fns';
 
 interface Props {
   userComments: Comment[];
@@ -6,14 +7,13 @@ interface Props {
 
 export default function AccountComments({ userComments }: Props) {
   return (
-    <div>
+    <>
       {userComments.map((comment: Comment) => (
-        <div key={comment.commentId}>
-          <h1>{comment.commentId}</h1>
-          <p>{comment.content}</p>
-          <p>{comment.author.username}</p>
+        <div key={comment.commentId} className='bg-sidebar rounded-xl flex flex-col items-start justify-center p-4 '>
+          <p className='text-[16px] pb-2'>{comment.content}</p>
+          <p className='text-foreground/24 text-xs italic'>{format(comment.createdAt, 'yyyy/MM/dd')}</p>
         </div>
       ))}
-    </div>
+    </>
   );
 }
