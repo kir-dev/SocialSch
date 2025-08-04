dff93bb00b73ef441041641bf4aa1d59207a2865
Sáfár Erik <safar.erik13@gmail.com>
2025-08-04 16:24:51 +0200

    Refactor LikesModule to include PrismaModule and export LikesService

--- apps/backend/src/likes/likes.module.ts
+++ apps/backend/src/likes/likes.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LikesService } from './likes.service';
 import { LikesController } from './likes.controller';
+import { PrismaModule } from '../prisma/prisma.module';
 
 @Module({
-  controllers: [LikesController],
+  imports: [PrismaModule],
   providers: [LikesService],
+  controllers: [LikesController],
+  exports: [LikesService],
 })
 export class LikesModule {}
