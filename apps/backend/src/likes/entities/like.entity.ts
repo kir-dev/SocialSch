49f26ad5b2b4610f55dec39b91f0dcd94e5bd96d
Sáfár Erik <safar.erik13@gmail.com>
2025-08-04 16:23:41 +0200

    Add Like entity with properties for likeId, userId, and postId

--- apps/backend/src/likes/entities/like.entity.ts
+++ apps/backend/src/likes/entities/like.entity.ts
@@ -1,1 +1,23 @@
-export class Like {}
+import { ApiProperty } from '@nestjs/swagger';
+import { User } from '../../users/entities/user.entity';
+import { Exclude } from 'class-transformer';
+import { Post } from '../../generated/prisma/client/client';
+
+export class Like {
+  @ApiProperty({ description: 'Unique identifier for the like' })
+  likeId: number;
+
+  @ApiProperty({ description: 'ID of the user who created the like' })
+  userId: string;
+
+  @ApiProperty({ description: 'User who created the like' })
+  @Exclude({ toPlainOnly: true })
+  user?: User;
+
+  @ApiProperty({ description: 'ID of the post that was liked' })
+  postId: number;
+
+  @ApiProperty({ description: 'Post that was liked' })
+  @Exclude({ toPlainOnly: true })
+  post?: Post;
+}
