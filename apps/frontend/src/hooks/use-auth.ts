4f5f27f0f049ad9bcadb07480b968a2ead92ea07
Sáfár Erik <safar.erik13@gmail.com>
2025-08-16 13:24:50 +0200

    Enhance useAuth hook to specify return type for improved type safety

--- apps/frontend/src/hooks/use-auth.ts
+++ apps/frontend/src/hooks/use-auth.ts
@@ -1,6 +1,6 @@
 import useProfile from './use-profile';
 
-export function useAuth() {
+export function useAuth(): { user: unknown; error: unknown; isLoading: boolean } {
   const { data: user, error, isLoading } = useProfile();
   return { user, error, isLoading };
 }
